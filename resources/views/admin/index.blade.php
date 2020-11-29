114de036a2bf92a3536d38f7ec1574cde5ee0f46
nikita <nikita@DESKTOP-EEKA4M8.localdomain>
2020-11-29 21:30:28 +0300

    tinymce

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,9 +2,52 @@
 
 @section('content')
 admin!
-<textarea name="content" id="editor"></textarea>
-<script src="{{ asset("ckeditor/ckeditor.js")}}"></script>
+<textarea name="content" id="edit"></textarea>
+<script src="{{asset('tinymce/tinymce.min.js')}}" ></script>
+
 <script type="text/javascript">
-  CKEDITOR.replace('editor');
+
+
+
+var route_prefix = "/filemanager";
+
+var editor_config = {
+      path_absolute : "",
+      selector: "textarea[name=content]",
+      plugins: [
+        "link image table fullscreen"
+      ],
+      toolbar: `fullscreen | undo redo | styleselect | bold italic | alignleft
+                aligncenter alignright alignjustify |
+                bullist numlist outdent indent | link image`,
+      language: 'ru',
+      //toolbar: 'table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
+      relative_urls: false,
+      height: 400,
+      width : "80%",
+      file_browser_callback : function(field_name, url, type, win) {
+        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        var cmsURL = editor_config.path_absolute + route_prefix + '?field_name=' + field_name;
+        if (type == 'image') {
+          cmsURL = cmsURL + "&type=Images";
+        } else {
+          cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.open({
+          file : cmsURL,
+          title : 'Filemanager',
+          width : x * 0.8,
+          height : y * 0.8,
+          resizable : "yes",
+          close_previous : "no"
+        });
+      }
+    };
+
+    tinymce.init(editor_config);
+
 </script>
 @endsection
